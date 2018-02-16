ec7cb7e2bc98577605a90e8ac4cbb80e5b0df69b
Larry Nesbit <larryjnesbit@gmail.com>
2018-02-16 06:40:28 -0600

    Addedd users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  
   	#resources :reviews
 	root "movies#index"
 
 	resources :movies do
 		resources :reviews
 	end
-  
+  	
+	resources :users
+  	get 'signup' => 'users#new'
 end
