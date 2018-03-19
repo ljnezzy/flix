c6e2f55546473490164a1c82b5260d816de4e746
Larry Nesbit <larryjnesbit@gmail.com>
2018-03-19 03:48:33 -0500

    Completed Level II exercise 11

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
 	def current_user
 	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
+
+	def page_title
+	    if content_for?(:title)
+	          content_tag(:title, "Flix - #{content_for(:title)}")
+	    else
+	      content_tag(:title, "Flix")
+	    end
+	end
+
+	def title(title)
+	  content_for(:title, title)
+	end
 end
